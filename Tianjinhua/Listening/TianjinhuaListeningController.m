e4cb01a615a68732f375399599006fed4b5265b2
Rui Du <durui109@gmail.com>
2012-05-25 21:25:19 -0400

    Listening -- Added result view

--- Tianjinhua/Listening/TianjinhuaListeningController.m
+++ Tianjinhua/Listening/TianjinhuaListeningController.m
@@ -9,6 +9,7 @@
 #import "TianjinhuaListeningController.h"
 #import "TianjinhuaListeningView.h"
 #import "TianjinhuaManualView.h"
+#import "TianjinhuaListeningResult.h"
 #import "TianjinhuaWords.h"
 #import "TianjinhuaWord.h"
 
@@ -18,13 +19,16 @@
 
 @property (weak, nonatomic) IBOutlet TianjinhuaListeningView *listeningView;
 @property (weak, nonatomic) IBOutlet TianjinhuaManualView *manualView;
+@property (weak, nonatomic) IBOutlet TianjinhuaListeningResult *resultView;
 @property (weak, nonatomic) IBOutlet UITextView *manualTextView;
-@property (weak, nonatomic) IBOutlet UILabel *word;
-@property (weak, nonatomic) IBOutlet UILabel *question;
-@property (weak, nonatomic) IBOutlet UIButton *choice1;
-@property (weak, nonatomic) IBOutlet UIButton *choice2;
-@property (weak, nonatomic) IBOutlet UIButton *choice3;
-@property (weak, nonatomic) IBOutlet UILabel *progress;
+@property (weak, nonatomic) IBOutlet UILabel *wordLabel;
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *choice1Btn;
+@property (weak, nonatomic) IBOutlet UIButton *choice2Btn;
+@property (weak, nonatomic) IBOutlet UIButton *choice3Btn;
+@property (weak, nonatomic) IBOutlet UILabel *progressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *commentLabel;
+@property (weak, nonatomic) IBOutlet UIButton *doneBtn;
 
 @property (strong, nonatomic) NSArray *words;
 @property (strong, nonatomic) NSArray *choices;
@@ -35,14 +39,17 @@
 @implementation TianjinhuaListeningController
 @synthesize listeningView = _listeningView;
 @synthesize manualView = _manualView;
+@synthesize resultView = _resultView;
 @synthesize manualTextView = _manualTextView;
 @synthesize words = _words;
-@synthesize word = _word;
-@synthesize question = _question;
-@synthesize choice1 = _choice1;
-@synthesize choice2 = _choice2;
-@synthesize choice3 = _choice3;
-@synthesize progress = _progress;
+@synthesize wordLabel = _wordLabel;
+@synthesize questionLabel = _questionLabel;
+@synthesize choice1Btn = _choice1Btn;
+@synthesize choice2Btn = _choice2Btn;
+@synthesize choice3Btn = _choice3Btn;
+@synthesize progressLabel = _progressLabel;
+@synthesize commentLabel = _commentLabel;
+@synthesize doneBtn = _doneBtn;
 @synthesize currentWordIdx = _currentWordIdx;
 @synthesize choices = _choices;
 
@@ -60,8 +67,9 @@
     [super viewDidLoad];
     self.listeningView.hidden = YES;
     self.manualView.hidden = YES;
-    //[self changeManualViewToHidden:NO];
-    [self changeListeningViewToHidden:NO];
+    self.resultView.hidden = YES;
+    [self changeManualViewToHidden:NO];
+    //[self changeListeningViewToHidden:NO];
 }
 
 - (void)viewDidUnload
@@ -69,12 +77,15 @@
     [self setListeningView:nil];
     [self setManualView:nil];
     [self setManualTextView:nil];
-    [self setWord:nil];
-    [self setQuestion:nil];
-    [self setChoice1:nil];
-    [self setChoice2:nil];
-    [self setChoice3:nil];
-    [self setProgress:nil];
+    [self setWordLabel:nil];
+    [self setQuestionLabel:nil];
+    [self setChoice1Btn:nil];
+    [self setChoice2Btn:nil];
+    [self setChoice3Btn:nil];
+    [self setProgressLabel:nil];
+    [self setDoneBtn:nil];
+    [self setResultView:nil];
+    [self setCommentLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -106,14 +117,17 @@
     if (currentWordIdx > CAPACITY - 1)
         currentWordIdx = CAPACITY - 1;
     
+    if (currentWordIdx == CAPACITY - 1)
+        self.doneBtn.hidden = FALSE;
+    
     _currentWordIdx = currentWordIdx;
-    self.progress.text = [NSString stringWithFormat:@"%d/%d",self.currentWordIdx+1, CAPACITY];
+    self.progressLabel.text = [NSString stringWithFormat:@"%d/%d",self.currentWordIdx+1, CAPACITY];
     TianjinhuaWord *word = [self.words objectAtIndex:_currentWordIdx];
-    self.word.text = word.word;
-    self.question.text = word.question;
-    [self setBtn:self.choice1 withTitle:[word.explanation objectAtIndex:0]];
-    [self setBtn:self.choice2 withTitle:[word.explanation objectAtIndex:1]];
-    [self setBtn:self.choice3 withTitle:[word.explanation objectAtIndex:2]];
+    self.wordLabel.text = word.word;
+    self.questionLabel.text = word.question;
+    [self setBtn:self.choice1Btn withTitle:[word.explanation objectAtIndex:0]];
+    [self setBtn:self.choice2Btn withTitle:[word.explanation objectAtIndex:1]];
+    [self setBtn:self.choice3Btn withTitle:[word.explanation objectAtIndex:2]];
     
     [self.listeningView setNeedsDisplay];
 }
@@ -164,10 +178,52 @@
         
         self.choices = [choices copy];
         self.currentWordIdx = 0;
-        self.progress.text = [NSString stringWithFormat:@"%d/%d",self.currentWordIdx+1, CAPACITY];
+        self.progressLabel.text = [NSString stringWithFormat:@"%d/%d",self.currentWordIdx+1, CAPACITY];
+        self.doneBtn.hidden = TRUE;
     }
     
     self.listeningView.hidden = hidden;
+}
+
+- (void)changeResultViewToHidden:(BOOL)hidden
+{
+    if (!hidden)
+    {
+    }
+    
+    self.resultView.hidden = hidden;
+}
+
+- (NSString *)commentForRatio:(float)ratio
+{
+    if(ratio > 0.8)
+        return @"你个天津娃，别以为我不知道";
+    else if(ratio > 0.6)
+        return @"你的天津话好犀利啊";
+    else if(ratio > 0.4)
+        return @"你的天津话不错";
+    else if(ratio > 0.2)
+        return @"你的天津话有待提高哈";
+    else {
+        return @"你是福兰银吧？";
+    }
+}
+
+- (IBAction)doneTouched:(UIButton *)sender
+{
+    // TODO -- check fill all answers.
+    float correct = 0.0;
+    for(int i=0; i<CAPACITY;i++)
+    {
+        TianjinhuaWord *word = [self.words objectAtIndex:i];
+        NSString *userAnswer = [self.choices objectAtIndex:i];
+        if([userAnswer isEqualToString:word.answer])
+            correct++;
+    }
+    
+    self.commentLabel.text = [self commentForRatio:correct/CAPACITY];
+    [self changeListeningViewToHidden:YES];
+    [self changeResultViewToHidden:NO];
 }
 
 - (IBAction)moveTouched:(UIButton *)sender
@@ -179,6 +235,14 @@
     else if ([sender.currentTitle isEqualToString:@"下一个"])
     {
         self.currentWordIdx = self.currentWordIdx + 1;
+    }
+    else if ([sender.currentTitle isEqualToString:@"分享"])
+    {
+    }
+    else if ([sender.currentTitle isEqualToString:@"再来一次"])
+    {
+        [self changeResultViewToHidden:YES];
+        [self changeListeningViewToHidden:NO];
     }
 }
 
---
+++ Tianjinhua/Listening/TianjinhuaListeningResult.h
@@ -0,0 +1,13 @@
+//
+//  TianjinhuaListeningResult.h
+//  Tianjinhua
+//
+//  Created by Xiaoxuan Zhang on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TianjinhuaListeningResult : UIView
+
+@end
